5d845384b1bd025883d8feff2fcff238c009ec32
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2022-04-18 12:47:00 -0700

    Update package.json and add index.ts

---
+++ generate_index.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm index.ts
+touch index.ts
+
+echo "export { Protobuf } from 'as-proto';" >> index.ts
+echo "" >> index.ts
+echo "export * from './google/protobuf/any';" >> index.ts
+echo "" >> index.ts
+
+#find koinos -name "*.ts" -type f | while read -d $'\0' file
+for file in $(find koinos -name "*.ts" -type f); do
+   dirname=${file%/*}
+   filename=${file##*/}
+   noext=${filename%.*}
+
+   echo "export * from './$dirname/$noext';" >> index.ts
+done
