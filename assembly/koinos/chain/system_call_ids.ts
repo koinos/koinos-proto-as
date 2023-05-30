dd4e79764e32be262ca831d817bff5fe3c7c278c
Koinos <67607993+koinos-ci@users.noreply.github.com>
2023-05-30 17:06:50 +0000

    Update for koinos-proto commit f85021b

--- assembly/koinos/chain/system_call_ids.ts
+++ assembly/koinos/chain/system_call_ids.ts
@@ -24,6 +24,7 @@ export namespace system_call_ids {
     set_account_nonce = 109,
     check_system_authority = 110,
     get_operation = 111,
+    get_contract_metadata = 112,
     get_account_rc = 201,
     consume_account_rc = 202,
     get_resource_limits = 203,
